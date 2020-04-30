0bf08e42e6a386e414b5f2cbc5efa95f298db452
meji <soy@meji.es>
2020-04-30 19:30:05 +0200

    Post dinámico

---
+++ components/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Page : React.FC = ({children}) => {
+    return (
+      <main>
+        {children}
+      </main>
+    )
+}
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,29 @@
-import React from "react";
-const Post: React.FC = ({Post: })=>{
-    return(
-        <main>{}</main>
-    )
+import React from 'react'
+import { Page } from '../../components/page'
+import { getAllPostIds, getPost } from '../../lib/posts'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { Post as PostModel } from '../../models/post'
+const Post: React.FC<{ post: PostModel }> = ({ post }) => (
+    <Page>
+        <h1>{post.title}</h1>
+        <small>{post.date}</small>
+        <div dangerouslySetInnerHTML={{ __html: post.contentHtml }}/>
+    </Page>
+)
+export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const id = params?.id as string
+    const post = await getPost(id)
+    return {
+        props: {
+            post
+        }
+    }
 }
 export default Post
